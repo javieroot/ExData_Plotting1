09afa1d19f0fcf7359b8ab15b9199aec632b786a
Javier <mac_javi@hotmail.com>
2014-05-10 17:49:55 -0500

    Esqueleto, primera y segunda gráficas

--- plots.R
+++ plots.R
@@ -1,5 +1,5 @@
-# # changing work directory
-#  setwd("/home/javier/Respaldo/Data Science Specialization/repositorios/ExData_Plotting1")
+# changing work directory
+# setwd("/home/javier/Respaldo/Data Science Specialization/repositorios/ExData_Plotting1")
 
 # Reading data
 datos <- read.table("household_power_consumption.txt",
@@ -9,11 +9,14 @@ datos <- read.table("household_power_consumption.txt",
 # data subset, only be using data from the dates 2007-02-01 and 2007-02-02
 datos <- datos[datos$Date == "1/2/2007" | datos$Date == "2/2/2007", ]
 
-# Changing Date column to date format
-datos$Date <- as.Date(datos$Date, "%d/%m/%Y")
-
 # Changing columns from text to numeric
 datos[, 3:9] <- sapply(datos[, 3:9], as.numeric)
+
+# Building the date full
+datos$date_full <- strptime(paste(datos$Date, datos$Time), "%d/%m/%Y %H:%M:%S")
+
+# Changing Date column to date format
+datos$Date <- as.Date(datos$Date, "%d/%m/%Y")
 
 
 # Plotting the graphics 1, 2, 3 y 4
--- scripts/plot2.R
+++ scripts/plot2.R
@@ -1,10 +1,11 @@
 # Save the plot
 png("./plots/plot2.png", height = 480, width = 480, units = "px", bg = "white")
 # Plot the data
-plot(paste(datos$Date,datos$Time),
+plot(datos$date_full,
      datos$Global_active_power,
+     type = "l",
      main = "Global Active Power",
-     xlab = NULL,
+     xlab = "",
      ylab = "Global Active power(kilowatts)")
 
 dev.off()
