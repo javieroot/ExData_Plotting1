89e221294ff70c7a10ea7daa4bbc5318af3fda0d
Javier <mac_javi@hotmail.com>
2014-05-11 16:08:41 -0500

    Versión final con formato

--- scripts/plot1.R
+++
@@ -1,8 +0,0 @@
-# Save the plot
-png("./plots/plot1.png", height = 480, width = 480, units = "px", bg = "transparent")
-# Plot the histogram
-hist(datos$Global_active_power,
-     main = "Global Active Power",
-     xlab = "Global Active Power(kilowatts)",
-     col  = "red")
-dev.off()
